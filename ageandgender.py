7cb35216afa6aa614bf6151cb3a8ec2da4da8739
meghnareddy-code <59131386+meghnareddy-code@users.noreply.github.com>
2021-06-20 15:07:22 +0530

    Update ageandgender.py

--- ageandgender.py
+++ ageandgender.py
@@ -27,10 +27,10 @@ faceProto = "opencv_face_detector.pbtxt"
 faceModel = "opencv_face_detector_uint8.pb"
 
 ageProto = "age_deploy.prototxt"
-ageModel = "https://github.com/GilLevi/AgeGenderDeepLearning/raw/master/models/age_net.caffemodel"
+ageModel = "age_net.caffemodel"
 
 genderProto = "gender_deploy.prototxt"
-genderModel = "https://github.com/GilLevi/AgeGenderDeepLearning/raw/master/models/gender_net.caffemodel"
+genderModel = "gender_net.caffemodel"
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
