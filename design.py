207c3e919e5b9851614b10e5036fd0151c1e313f
Thirumala Inumula <82645910+18241A05W2@users.noreply.github.com>
2021-08-07 22:08:38 -0900

    Update design.py

--- design.py
+++ design.py
@@ -3,15 +3,29 @@ html_temp = """
     <html lang="en" dir="ltr">
     <style>
         .reportview-container {
-            background: black
+            background:url(https://thumbs.dreamstime.com/z/people-gender-race-orientation-age-set-black-background-people-gender-race-orientation-age-set-black-background-icons-131394019.jpg);
       }
+        h1{
+           background-color:LightGray;
+           color:Orange;
+           text-align:Center;
+           }
+        p{
+          color:maroon;
+          text-align:center;
+          font-family:"Brush Script MT";
+         }
+        div{
+            background:url(2wCEAAoHCBISEhIREhIRERESEhIREBISERERERESGBgaGhgUGBgcIS4lHB4rIBgYJkYmKy8xNTY1GiQ7QDszPy40NTEBDAwMEA8QHxISHzQrJCE0MTExNDQ0NDQ2NDQxMTQxNDE0NTQxNDFANDE0MTE0NDQ0MTE0NDUxNDQ0MTE0NDExMf);
+            padding:5px
+        }
     </style>
     <head>
         <meta charset="utf-8">
         <title>Age and Gender Detection</title>
     </head>
     <body style="background-color:red;">
-        <div style="background-color:teal ;padding:10px">
+        <div style="background-color:teal ;padding:10px;border-radius:15px 50px;border-style:double">
         <h2 style="color:white;text-align:center;">Age and Gender Detection webapp</h2>
         </div>
     </body>
